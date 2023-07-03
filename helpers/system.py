00c0262bca16131b722a5d1bc38aaacd5fac2fd3
Earl Cameron <mr.e.cameron@gmail.com>
2023-07-02 21:13:55 -0400

    func to class

--- helpers/system.py
+++ helpers/system.py
@@ -1,5 +1,6 @@
 import time
 
+
 def track_time(func):
     """
     Decorator to track the time taken by a function and print the elapsed time.
@@ -14,3 +15,29 @@ def track_time(func):
         return result
 
     return wrapper
+
+
+# This is the outer decorator function. It accepts an object 'obj' that we want to pass to the function being decorated.
+def insert_app(obj):
+
+    # The decorator function accepts the function to be decorated as its argument.
+    def decorator(func):
+
+        # The wrapper function will replace the original function when the decorator is applied. It accepts any number of 
+        # positional arguments (*args) and keyword arguments (**kwargs) so it can be used with any function, regardless of
+        # the function's signature.
+        def wrapper(*args, **kwargs):
+
+            # Before the decorated function is called, we print a message to indicate that the object is being added to the function.
+            print(f"Object: {obj} has been added to the function: {func.__name__}")
+
+            # We call the decorated function with the object as the first argument, followed by the other arguments that were passed in. 
+            # The return value of the original function is returned by the wrapper.
+            return func(obj, *args, **kwargs)
+
+        # The decorator function returns the wrapper function. It has the same name as the original function but its behavior has been modified.
+        return wrapper
+
+    # The outer decorator function returns the decorator function.
+    return decorator
+
--- index.py
+++ index.py
@@ -6,105 +6,78 @@ from step_2_audio_recording.index import listen_and_save_audio
 from step_3_stt.index import transcribe, load_model as load_model_stt
 from step_4_chatbot.index import ChatGPT
 from step_5_tts_fast.index import generate_audio, load_model as load_model_tts
-from step_5_tts_slow.index import generate_audio as generate_audio_v2
 from step_7_image_generation.index import generate_image
+from helpers.system import insert_app
 
-# from server.index import app
-
-# Load the environment variables from the .env file
 load_dotenv()
 
-OUTPUT = os.getenv("OUTPUT")
-model = os.getenv("MODEL")
-aiRole = os.getenv("AIROLE")
-userRole = os.getenv("USERROLE")
 
-# Create ChatGPT instance
-chatGPT = ChatGPT(model, aiRole, userRole)
+class ChatApp:
+    def __init__(self):
+        self.OUTPUT = os.getenv("OUTPUT")
+        self.model = os.getenv("MODEL")
+        self.aiRole = os.getenv("AIROLE")
+        self.userRole = os.getenv("USERROLE")
+        self.initialize_app()
 
-# Models
-APP = {}
+    def initialize_app(self):
+        device = os.getenv("STTDEVICE")
+        self.stt = load_model_stt(device)
+        device = os.getenv("TTSDEVICE")
+        self.tts = load_model_tts(device)
+        self.audio_player = AudioPlayer()
+        self.chat = ChatGPT(self.model, self.aiRole, self.userRole)
 
-chatInProgress = False
+    def process_context(self):
+        if self.context == "porcupine":
+            print("Resetting ChatGPT History")
+            self.chat.reset_history()
+            return False
+        elif self.context == "americano":
+            return False
+        else:
+            self.audio_player.play_audio_file_sync("resources/notif1.wav")
+            return True
 
-
-def load_models():
-    device = os.getenv("STTDEVICE")
-    APP['stt'] = load_model_stt(device)
-    device = os.getenv("TTSDEVICE")
-    APP['tts'] = load_model_tts(device)
-    APP['audio_player'] = AudioPlayer()
-
-
-# main function
-def main(context):
-    # getting the context of the wake word
-    # prevent running simultaneously
-    global chatInProgress
-    if chatInProgress:
-        return
-    else:
-        chatInProgress = True
-
-    # needs custom models for other non-standard wake words
-    if context == "porcupine":
-        print("Resetting ChatGPT History")
-        chatGPT.reset_history()
-    elif context == "americano":
-        pass
-    else:
-        # play audio buzz to show that the computer is listening after wakeword
-        APP['audio_player'].play_audio_file_sync("resources/notif1.wav")
-
-        # step 2 record audio and end when stop speaking
-        file_path = f"{OUTPUT}/recording.wav"
+    def record_audio(self):
+        file_path = f"{self.OUTPUT}/recording.wav"
         listen_and_save_audio(file_path)
-        # audio = listen_and_save_audio()
+        return file_path
 
-        # step 3 speech to text
-        prompt = transcribe(file_path=file_path, model=APP['stt'])
-        # prompt = transcribe(wav_data=audio)
-        # global app
-        # app.message_history = chatGPT.history
-        message_history = chatGPT.history
+    def convert_audio_to_text(self, file_path):
+        return transcribe(file_path=file_path, model=self.stt)
 
-        # step 4 prompt chatGPT
-        print(f'{chatGPT.user_role}:{prompt}')
-        response = chatGPT.next(prompt)
-        print(f'{chatGPT.ai_role}: {response}')
-        # app.message_history = chatGPT.history
+    def get_chat_response(self, prompt):
+        print(f'{self.chat.user_role}:{prompt}')
+        response = self.chat.next(prompt)
+        print(f'{self.chat.ai_role}: {response}')
+        return response
 
-        # step 5 generate speech to text
+    def play_audio_response(self, response):
         filePath = generate_audio(
-            prompt=response, output_path=OUTPUT, model=APP["tts"])
-        # filePath = generate_audio_v2(response, cuda_device=2)
-        # play_audio_file(filePath)
-        player = APP['audio_player'].play_audio_file_sync(filePath)
+            prompt=response, output_path=self.OUTPUT, model=self.tts)
+        self.player = self.audio_player.play_audio_file_async(filePath)
 
-        # allow new prompts
-        chatInProgress = False
+    def main(self, context):
+        self.context = context
 
-    # play audio buzz to show that the computer is ready to start listening
-    APP['audio_player'].play_audio_file_sync("resources/notif2.wav")
+        if not self.process_context():
+            self.chatInProgress = False
+            return
 
-    # step 7
-    # prompt = "a photo of an astronaut riding a horse on mars"
-    # filename = "test.png"
-    # generate_image(prompt, OUTPUT, filename)
+        file_path = self.record_audio()
+        prompt = self.convert_audio_to_text(file_path)
+        response = self.get_chat_response(prompt)
+        self.play_audio_response(response)
 
+    def listen_for_wake_word(self):
+        print("Computer is listening, try 'Computer' to make a prompt or 'porcupine' to start a new conversation")
+        detect_hotword(self.main)
 
-def listenForWakeWord():
-    # Perform other tasks in the main thread
-    print("Computer is listening, try 'Computer' to make a prompt or 'porcupine' to start a new conversation")
-    detect_hotword(main)
+    def run(self):
+        self.listen_for_wake_word()
 
 
 if __name__ == "__main__":
-    # Start Flask app in a new thread
-    # wakeWord = threading.Thread(target=listenForWakeWord)
-    # wakeWord.start()
-    # main('')
-    load_models()
-    listenForWakeWord()
-
-    # app.run(debug=True)
+    app = ChatApp()
+    app.run()
